e1e9e15792cd2a043957d7c0dbdd2710363d79b8
Simple <591831866@qq.com>
2021-04-18 00:45:38 +0800

    init

---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <div id="nav">
+      <router-link to="/myForm">MyForm</router-link>
+    </div>
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
---
+++ src/views/MyForm.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>The page should stay here all the time, and the browser address will not change</div>
+</template>
+
+<script>
+
+  export default {
+    name: 'MyForm',
+    beforeRouteLeave(to, from, next) {
+      const rest = window.confirm('Do you want to leave?')
+      if (rest) {
+        next()
+      } else {
+        next(false)
+      }
+    }
+  }
+</script>
